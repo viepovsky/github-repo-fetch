62ee3a4c7905b7490978c94ac693aa0a94e200df
Oskar R <o.rajzner@gmail.com>
2024-02-19 01:02:20 +0100

    Fix problem after adding @Builder needs another constructors in dtos

--- src/main/java/com/viepovsky/client/dto/BranchDTO.java
+++ src/main/java/com/viepovsky/client/dto/BranchDTO.java
@@ -2,13 +2,17 @@ package com.viepovsky.client.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Map;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class BranchDTO {
 
--- src/main/java/com/viepovsky/client/dto/OwnerDTO.java
+++ src/main/java/com/viepovsky/client/dto/OwnerDTO.java
@@ -2,11 +2,15 @@ package com.viepovsky.client.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class OwnerDTO {
 
