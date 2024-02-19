6b77c4d4cc39227c996ecef943e90cfe740231cc
Oskar R <o.rajzner@gmail.com>
2024-02-19 23:18:02 +0100

    Change RestTemplate to RestClient in GitHubClient class

--- src/main/java/com/viepovsky/client/GithubApiClient.java
+++ src/main/java/com/viepovsky/client/GithubApiClient.java
@@ -4,60 +4,38 @@ import com.viepovsky.client.dto.BranchDTO;
 import com.viepovsky.client.dto.GithubRepositoryDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-
-import static java.util.Optional.ofNullable;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class GithubApiClient {
 
-    private static final String ACCEPT_HEADER_GITHUB_JSON = "application/vnd.github+json";
-
-
-    private final RestTemplate restTemplate;
+    private final RestClient restClient;
 
     private final GithubApiConfig githubApiConfig;
 
     public List<GithubRepositoryDTO> getAllRepositoriesByUsername(String username) {
-        HttpHeaders header = buildHeader();
-        HttpEntity<String> requestEntityHeaders = new HttpEntity<>(header);
         URI url = buildUserUrl(username);
         try {
-            ResponseEntity<GithubRepositoryDTO[]> responseEntity = restTemplate.exchange(
-                    url,
-                    HttpMethod.GET,
-                    requestEntityHeaders,
-                    GithubRepositoryDTO[].class
-            );
-            return Arrays.asList(ofNullable(responseEntity.getBody()).orElse(new GithubRepositoryDTO[0]));
-        } catch (HttpClientErrorException e) {
-            log.error("Error while getting repositories. " + e.getMessage());
-            throw e;
+            return restClient.get()
+                             .uri(url)
+                             .accept(MediaType.APPLICATION_JSON)
+                             .retrieve()
+                             .body(new ParameterizedTypeReference<>() {
+                             });
         } catch (RestClientException e) {
             log.error("Error while getting repositories. " + e.getMessage());
-            return Collections.emptyList();
+            throw e;
         }
-    }
-
-    private HttpHeaders buildHeader() {
-        HttpHeaders header = new HttpHeaders();
-        header.set("Accept", ACCEPT_HEADER_GITHUB_JSON);
-        return header;
     }
 
     private URI buildUserUrl(String username) {
@@ -79,32 +57,27 @@ public class GithubApiClient {
     }
 
     public GithubRepositoryDTO getAllBranchesAndLastCommits(GithubRepositoryDTO repository) {
-        HttpHeaders header = buildHeader();
-        HttpEntity<String> requestEntityHeaders = new HttpEntity<>(header);
         URI url = buildRepoUrl(
                 repository.repositoryOwner()
                           .login(),
                 repository.repositoryName()
         );
         try {
-            ResponseEntity<BranchDTO[]> responseEntity = restTemplate.exchange(
-                    url,
-                    HttpMethod.GET,
-                    requestEntityHeaders,
-                    BranchDTO[].class
-            );
-
-            List<BranchDTO> branches = Arrays.asList(ofNullable(responseEntity.getBody()).orElse(new BranchDTO[0]));
-
+            List<BranchDTO> response = restClient.get()
+                                                 .uri(url)
+                                                 .accept(MediaType.APPLICATION_JSON)
+                                                 .retrieve()
+                                                 .body(new ParameterizedTypeReference<>() {
+                                                 });
             return GithubRepositoryDTO.builder()
                                       .repositoryName(repository.repositoryName())
                                       .repositoryOwner(repository.repositoryOwner())
                                       .isFork(repository.isFork())
-                                      .branches(branches)
+                                      .branches(response)
                                       .build();
         } catch (RestClientException e) {
             log.error("Error while getting branches." + e.getMessage(), e);
-            return repository;
+            throw e;
         }
     }
 }
--- src/main/java/com/viepovsky/client/GithubApiConfig.java
+++ src/main/java/com/viepovsky/client/GithubApiConfig.java
@@ -4,15 +4,16 @@ import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 
 @Getter
 @Configuration
 public class GithubApiConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestClient restClient() {
+        return RestClient.builder()
+                         .build();
     }
 
     @Value("${github.api.endpoint}")
